1b45e26db515bb33650d83666a4bc60547499b55
anneau <hkobori@gmx.com>
2024-11-30 08:29:56 +0000

    🔧 Add storybook base settings

---
+++ app/page.stories.tsx
@@ -0,0 +1,12 @@
+import { Meta, StoryObj } from '@storybook/react'
+
+import Page from './page'
+
+const meta: Meta<typeof Page> = {
+  component: Page,
+}
+export default meta
+
+type Story = StoryObj<typeof Page>
+
+export const Example: Story = {}
